a54526fae65d7f2020b133d3794ba4c5821b4a39
JasonX716 <56998002+JasonX716@users.noreply.github.com>
2020-12-01 14:57:51 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const path = require('path');
+const router = express.Router();
+const PORT = 3001;
+
+const app = express();
+
+const db = mysql.createConnection({
+    user:'root',
+    host:'localhost',
+    password:'password',
+    database:'dryer_tracker'
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use('/api', router);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.post("/create", (req, res) => {
+    const serialNumber = req.body.serialNumber;
+    const buildDate = req.body.buildDate;
+    const status = req.body.status;
+
+    db.query(
+        'INSERT INTO dryers (serialNumber, buildDate, status) VALUES (?,?,?)', [serialNumber, buildDate, status], (err, result) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.send("values inserted");
+        }
+    }
+    );
+});
+
+app.get("/dryers", (req, res) => {
+    db.query('SELECT * FROM dryers', (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+
+app.put("/update", (req, res) => {
+    const id = req.body.id;
+    const status = req.body.status;
+    db.query(
+     'UPDATE dryers SET status = ? WHERE id = ?',
+     [status, id], 
+     (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+})
+
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id
+    db.query("DELETE FROM dryers WHERE id = ?", id, (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send(result);
+        }
+    });
+})
+
+
+
+/* app.delete() */
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
